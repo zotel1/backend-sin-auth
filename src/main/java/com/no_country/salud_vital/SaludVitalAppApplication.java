7de322206ba469628baa5c3d6152246dcb33a898
zotel1 <zotelsigel@gmail.com>
2024-09-14 17:25:04 -0300

    update cors v9

--- src/main/java/com/no_country/salud_vital/SaludVitalAppApplication.java
+++ src/main/java/com/no_country/salud_vital/SaludVitalAppApplication.java
@@ -2,8 +2,10 @@ package com.no_country.salud_vital;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+//@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class SaludVitalAppApplication {
 
 	public static void main(String[] args) {
