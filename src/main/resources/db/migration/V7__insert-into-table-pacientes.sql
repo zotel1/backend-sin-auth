0df33b78941ba0fb4d346c49a3f5ae4e9d82c279
zotel1 <zotelsigel@gmail.com>
2024-09-18 18:47:16 -0300

    Correccion migraciones pacientes

--- src/main/resources/db/migration/V7__insert-into-table-pacientes.sql
+++ src/main/resources/db/migration/V7__insert-into-table-pacientes.sql
@@ -1,16 +1,14 @@
 DELETE FROM `railway`.`pacientes`;
 
 INSERT INTO `railway`.`pacientes` (`id`, `nombre`, `email`, `documento`, `calle`, `distrito`, `complemento`, `numero`, `ciudad`, `telefono`, `activo`)
-VALUES ('1', 'amanda', 'a@mail.com', '1111111', 'ran', 'ran', '900', '900', 'ran', '12334567', '1');
+VALUES ('1', 'Amanda Pérez', 'amandaPer@mail.com', '39476287', 'Florida', 'Misiones', '900', '900', 'Buenos Aires', '3517789075', '1');
 INSERT INTO `railway`.`pacientes` (`id`, `nombre`, `email`, `documento`, `calle`, `distrito`, `complemento`, `numero`, `ciudad`, `telefono`, `activo`)
-VALUES ('2', 'bruna', 'b@mail.com', '2222222', 'ran', 'ran', '900', '900', 'ran', '12334567', '1');
+VALUES ('2', 'Bruna González', 'brunitaG@mail.com', '25988737', 'San Martín', 'Mendoza', '900', '900', 'Mendoza', '3794278364', '1');
 INSERT INTO `railway`.`pacientes` (`id`, `nombre`, `email`, `documento`, `calle`, `distrito`, `complemento`, `numero`, `ciudad`, `telefono`, `activo`)
-VALUES ('3', 'carla', 'c@mail.com', '3333333', 'ran', 'ran', '900', '900', 'ran', '12334567', '1');
+VALUES ('3', 'Carla Ramírez', 'carlotaRRR@mail.com', '40876209', 'Corrientes', 'Salta', '900', '900', 'Salta', '3538768210', '1');
 INSERT INTO `railway`.`pacientes` (`id`, `nombre`, `email`, `documento`, `calle`, `distrito`, `complemento`, `numero`, `ciudad`, `telefono`, `activo`)
-VALUES ('4', 'debora', 'd@mail.com', '4444444', 'ran', 'ran', '900', '900', 'ran', '12334567', '1');
+VALUES ('4', 'Debora Martínez', 'debbbi@mail.com', '41344587', 'Rivadavia', 'Santa Fe', '900', '900', 'Rosario', '3771876230', '1');
 INSERT INTO `railway`.`pacientes` (`id`, `nombre`, `email`, `documento`, `calle`, `distrito`, `complemento`, `numero`, `ciudad`, `telefono`, `activo`)
-VALUES ('5', 'esmeralda', 'e@mail.com', '5555555', 'ran', 'ran', '900', '900', 'ran', '12334567', '1');
+VALUES ('5', 'Esmeralda Hernández', 'esmiwayne@mail.com', '56768729', '9 de Julio', 'Misiones', '900', '900', 'Posadas', '3704876529', '1');
 INSERT INTO `railway`.`pacientes` (`id`, `nombre`, `email`, `documento`, `calle`, `distrito`, `complemento`, `numero`, `ciudad`, `telefono`, `activo`)
-VALUES ('6', 'francis', 'f@mail.com', '6666666', 'ran', 'ran', '900', '900', 'ran', '12334567', '1');
-
-
+VALUES ('6', 'Francis Cano', 'francis_capo@mail.com', '66433598', 'Sarmiento', 'San Juan', '900', '900', 'San Juan', '1134569230', '1');
