54f8bea1935e961617e9ffdd9c4aeaddd5189708
zotel1 <zotelsigel@gmail.com>
2024-09-18 23:30:09 -0300

    update migraciones v8 v7

--- src/main/resources/db/migration/V1__create-table-medicos.sql
+++ src/main/resources/db/migration/V1__create-table-medicos.sql
@@ -3,8 +3,8 @@ create table medicos(
 
     id bigint not null auto_increment,
     nombre varchar(100) not null,
-    email varchar(100) not null unique,
-    documento varchar(25) not null unique,
+    email varchar(100) not null,
+    documento varchar(25) not null,
     especialidad varchar(100) not null,
     calle varchar(100) not null,
     distrito varchar(100) not null,
