a26fb59e6081c5a75dc8fcb889b2c8bb820aba3c
zotel1 <zotelsigel@gmail.com>
2024-09-18 23:40:48 -0300

    update migraciones tablas

--- src/main/resources/db/migration/V8__insert-into-table-medicos.sql
+++ src/main/resources/db/migration/V8__insert-into-table-medicos.sql
@@ -1,34 +1,49 @@
 DELETE FROM `railway`.`medicos`;
 
 INSERT INTO `railway`.`medicos` (`id`, `nombre`, `email`, `documento`, `especialidad`, `calle`, `distrito`, `complemento`, `numero`, `ciudad`, `telefono`, `activo`)
-VALUES ('1', 'Antonio Rios', 'antoniosaul@gmail.com', '35988675', 'GENERAL', 'b', 'b', 'b', 'b', 'b', 'b', '1');
+VALUES ('1', 'Antonio Rios', 'antoniosaul@gmail.com', '35988675', 'GENERAL', 'Florida', 'Misiones', '900', '900', 'Buenos Aires', '3517789075', '1');
 INSERT INTO `railway`.`medicos` (`id`, `nombre`, `email`, `documento`, `especialidad`, `calle`, `distrito`, `complemento`, `numero`, `ciudad`, `telefono`, `activo`)
-VALUES ('2', 'Bernardo  Caballero', 'berniber@gmail.com', '21242121', 'GENERAL', 'b', 'b', 'b', 'b', 'b', 'b', '1');
+VALUES ('2', 'Bernardo  Caballero', 'berniber@gmail.com', '21242121', 'GENERAL', 'San Martín', 'Mendoza', '900', '900', 'Mendoza', '3794278364', '1');
 INSERT INTO `railway`.`medicos` (`id`, `nombre`, `email`, `documento`, `especialidad`, `calle`, `distrito`, `complemento`, `numero`, `ciudad`, `telefono`, `activo`)
-VALUES ('3', 'Carlos Perez', 'carlitospc@gmail.com', '23247323', 'CARDIOLOGIA', 'b', 'b', 'b', 'b', 'b', 'b', '0');
+VALUES ('3', 'Carlos Perez', 'carlitospc@gmail.com', '23247323', 'GENERAL', 'Corrientes', 'Salta', '900', '900', 'Salta', '3538768210', '1');
 INSERT INTO `railway`.`medicos` (`id`, `nombre`, `email`, `documento`, `especialidad`, `calle`, `distrito`, `complemento`, `numero`, `ciudad`, `telefono`, `activo`)
-VALUES ('4', 'Guts Palacios', 'guts@gmail.com', '32783232', 'CARDIOLOGIA' , 'b', 'b', 'b', 'b', 'b', 'b', '1');
+VALUES ('4', 'Guts Palacios', 'guts@gmail.com', '32783232', 'CARDIOLOGIA', 'Rivadavia', 'Santa Fe', '900', '900', 'Rosario', '3771876230', '1');
 INSERT INTO `railway`.`medicos` (`id`, `nombre`, `email`, `documento`, `especialidad`, `calle`, `distrito`, `complemento`, `numero`, `ciudad`, `telefono`, `activo`)
-VALUES ('5', 'Eduardo Alfonso', 'eeducapo@gmail.com', '34394344', 'GINECOLOGIA', 'b', 'b', 'b', 'b', 'b', 'b', '1');
+VALUES ('5', 'Eduardo Alfonso', 'eeducapo@gmail.com', '34394344', 'CARDIOLOGIA', '9 de Julio', 'Misiones', '900', '900', 'Posadas', '3704876529', '1');
 INSERT INTO `railway`.`medicos` (`id`, `nombre`, `email`, `documento`, `especialidad`, `calle`, `distrito`, `complemento`, `numero`, `ciudad`, `telefono`, `activo`)
-VALUES ('6', 'Ana Gómez', 'anitag@gmail.com', '32932302', 'GINECOLOGIA', 'b', 'b', 'b', 'b', 'b', 'b', '1');
+VALUES ('6', 'Fernando Castelli', 'ferchuf@gmail.com', '43451341', 'CARDIOLOGIA', 'Sarmiento', 'San Juan', '900', '900', 'San Juan', '1134569230',  '1');
 INSERT INTO `railway`.`medicos` (`id`, `nombre`, `email`, `documento`, `especialidad`, `calle`, `distrito`, `complemento`, `numero`, `ciudad`, `telefono`, `activo`)
-VALUES ('7', 'Fernando Castelli', 'ferchuf@gmail.com', '43451341', 'NEUROLOGIA', 'b', 'b', 'b', 'b', 'b', 'b', '1');
+VALUES ('7', 'Ana Gómez', 'anitag@gmail.com', '32932302', 'GINECOLOGIA', 'Rivadavia', 'Santa Fe', '900', '900', 'Rosario', '3771876230', '1');
 INSERT INTO `railway`.`medicos` (`id`, `nombre`, `email`, `documento`, `especialidad`, `calle`, `distrito`, `complemento`, `numero`, `ciudad`, `telefono`, `activo`)
-VALUES ('8', 'Pedro López', 'pedricr7@gmail.com', '32476132', 'NEUROLOGIA', 'b', 'b', 'b', 'b', 'b', 'b', '1');
+VALUES ('8', 'Pedro López', 'pedricr7@gmail.com', '32476132', 'GINECOLOGIA', '9 de Julio', 'Misiones', '900', '900', 'Posadas', '3704876529', '1');
 INSERT INTO `railway`.`medicos` (`id`, `nombre`, `email`, `documento`, `especialidad`, `calle`, `distrito`, `complemento`, `numero`, `ciudad`, `telefono`, `activo`)
-VALUES ('9', 'Lucía Díaz', 'luluD@gmail.com', '42454887', 'NUTRICION', 'b', 'b', 'b', 'b', 'b', 'b', '1');
+VALUES ('9', 'Lucía Díaz', 'luluD@gmail.com', '42454887', 'GINECOLOGIA', 'Sarmiento', 'San Juan', '900', '900', 'San Juan', '1134569230',  '1');
 INSERT INTO `railway`.`medicos` (`id`, `nombre`, `email`, `documento`, `especialidad`, `calle`, `distrito`, `complemento`, `numero`, `ciudad`, `telefono`, `activo`)
-VALUES ('10', 'Carlos Mendoza', 'carlitosss@gmail.com', '328745092', 'NUTRICION', 'b', 'b', 'b', 'b', 'b', 'b', '1');
+VALUES ('10', 'Carlos Mendoza', 'carlitosss@gmail.com', '328745092', 'NEUROLOGIA', 'Rivadavia', 'Santa Fe', '900', '900', 'Rosario', '3771876230', '1');
 INSERT INTO `railway`.`medicos` (`id`, `nombre`, `email`, `documento`, `especialidad`, `calle`, `distrito`, `complemento`, `numero`, `ciudad`, `telefono`, `activo`)
-VALUES ('11', 'María Sánchez', 'merys@gmail.com', '34285610', 'ORTOPEDIA', 'b', 'b', 'b', 'b', 'b', 'b', '1');
+VALUES ('11', 'María Sánchez', 'merys@gmail.com', '34285610', 'NEUROLOGIA', '9 de Julio', 'Misiones', '900', '900', 'Posadas', '3704876529', '1');
 INSERT INTO `railway`.`medicos` (`id`, `nombre`, `email`, `documento`, `especialidad`, `calle`, `distrito`, `complemento`, `numero`, `ciudad`, `telefono`, `activo`)
-VALUES ('12', 'Jorge Pérez', 'jorgito@gmail.com', '43792387', 'ORTOPEDIA', 'b', 'b', 'b', 'b', 'b', 'b', '1');
+VALUES ('12', 'Jorge Pérez', 'jorgito@gmail.com', '43792387', 'NEUROLOGIA', 'Sarmiento', 'San Juan', '900', '900', 'San Juan', '1134569230',  '1');
 INSERT INTO `railway`.`medicos` (`id`, `nombre`, `email`, `documento`, `especialidad`, `calle`, `distrito`, `complemento`, `numero`, `ciudad`, `telefono`, `activo`)
-VALUES ('13', 'Andrés Ramos', 'andresitos@gmail.com', '36872196', 'PEDIATRIA', 'b', 'b', 'b', 'b', 'b', 'b', '1');
+VALUES ('13', 'Laura García', 'laulauid@gmail.com', '37926107', 'NUTRICION', 'Rivadavia', 'Santa Fe', '900', '900', 'Rosario', '3771876230', '1');
 INSERT INTO `railway`.`medicos` (`id`, `nombre`, `email`, `documento`, `especialidad`, `calle`, `distrito`, `complemento`, `numero`, `ciudad`, `telefono`, `activo`)
-VALUES ('14', 'Roberto Gómez', 'robertitos@gmail.com', '43237391', 'PEDIATRIA', 'b', 'b', 'b', 'b', 'b', 'b', '1');
+VALUES ('14', 'Fernando Martínez', 'ferchuelmejor@gmail.com', '38654209', 'NUTRICION', '9 de Julio', 'Misiones', '900', '900', 'Posadas', '3704876529', '1');
 INSERT INTO `railway`.`medicos` (`id`, `nombre`, `email`, `documento`, `especialidad`, `calle`, `distrito`, `complemento`, `numero`, `ciudad`, `telefono`, `activo`)
-VALUES ('15', 'Daniel Castro', 'danielito@gmail.com', '35897215', 'UROLOGIA', 'b', 'b', 'b', 'b', 'b', 'b', '1');
+VALUES ('15', 'Sofía Rodríguez', 'sofilomasf@gmail.com', '39872196', 'NUTRICION', 'Sarmiento', 'San Juan', '900', '900', 'San Juan', '1134569230',  '1');
 INSERT INTO `railway`.`medicos` (`id`, `nombre`, `email`, `documento`, `especialidad`, `calle`, `distrito`, `complemento`, `numero`, `ciudad`, `telefono`, `activo`)
-VALUES ('16', 'Verónica Ortiz', 'veritof@gmail.com', '40982348', 'UROLOGIA, 'b', 'b', 'b', 'b', 'b', 'b', '1');
+VALUES ('16', 'Elena Fernández', 'elielia@gmail.com', '398756178', 'ORTOPEDIA', 'Rivadavia', 'Santa Fe', '900', '900', 'Rosario', '3771876230', '1');
+INSERT INTO `railway`.`medicos` (`id`, `nombre`, `email`, `documento`, `especialidad`, `calle`, `distrito`, `complemento`, `numero`, `ciudad`, `telefono`, `activo`)
+VALUES ('17', 'Miguel Torres', 'miguelitoliru@gmail.com', '40876329', 'ORTOPEDIA', '9 de Julio', 'Misiones', '900', '900', 'Posadas', '3704876529', '1');
+INSERT INTO `railway`.`medicos` (`id`, `nombre`, `email`, `documento`, `especialidad`, `calle`, `distrito`, `complemento`, `numero`, `ciudad`, `telefono`, `activo`)
+VALUES ('18', 'Patricia Vega', 'patrisr@gmail.com', '42449321', 'ORTOPEDIA', 'Sarmiento', 'San Juan', '900', '900', 'San Juan', '1134569230',  '1');
+INSERT INTO `railway`.`medicos` (`id`, `nombre`, `email`, `documento`, `especialidad`, `calle`, `distrito`, `complemento`, `numero`, `ciudad`, `telefono`, `activo`)
+VALUES ('19', 'Andrés Ramos', 'andresitos@gmail.com', '36872196', 'PEDIATRIA', 'Rivadavia', 'Santa Fe', '900', '900', 'Rosario', '3771876230', '1');
+INSERT INTO `railway`.`medicos` (`id`, `nombre`, `email`, `documento`, `especialidad`, `calle`, `distrito`, `complemento`, `numero`, `ciudad`, `telefono`, `activo`)
+VALUES ('20', 'Claudia Moreno', 'laclau@gmail.com', '37893201', 'PEDIATRIA', '9 de Julio', 'Misiones', '900', '900', 'Posadas', '3704876529', '1');
+INSERT INTO `railway`.`medicos` (`id`, `nombre`, `email`, `documento`, `especialidad`, `calle`, `distrito`, `complemento`, `numero`, `ciudad`, `telefono`, `activo`)
+VALUES ('21', 'Roberto Gómez', 'robertitos@gmail.com', '43237391', 'PEDIATRIA', 'Sarmiento', 'San Juan', '900', '900', 'San Juan', '1134569230',  '1');
+INSERT INTO `railway`.`medicos` (`id`, `nombre`, `email`, `documento`, `especialidad`, `calle`, `distrito`, `complemento`, `numero`, `ciudad`, `telefono`, `activo`)
+VALUES ('22', 'Isabel Ruiz', 'isabelabuena@gmail.com', '38981120', 'UROLOGIA', 'Rivadavia', 'Santa Fe', '900', '900', 'Rosario', '3771876230', '1');
+INSERT INTO `railway`.`medicos` (`id`, `nombre`, `email`, `documento`, `especialidad`, `calle`, `distrito`, `complemento`, `numero`, `ciudad`, `telefono`, `activo`)
+VALUES ('23', 'Daniel Castro', 'danielito@gmail.com', '35897215', 'UROLOGIA', '9 de Julio', 'Misiones', '900', '900', 'Posadas', '3704876529', '1');
+
