0219dc90ee3ce910c061d4cc8f7d04e4fa9a8e3a
zotel1 <zotelsigel@gmail.com>
2024-09-19 21:17:47 -0300

    update agregamos los datos antiguos a la tabla consultas doctores y especialidades

--- src/main/resources/db/migration/V9__insert-into-table-consultas.sql
+++ src/main/resources/db/migration/V9__insert-into-table-consultas.sql
@@ -1,20 +1,20 @@
 INSERT INTO railway.consultas (medico_id, paciente_id, fecha, motivo_cancelamiento)
 VALUES ('1', '1', '2024-07-05 09:00:00', NULL);
 INSERT INTO railway.consultas (medico_id, paciente_id, fecha, motivo_cancelamiento)
-VALUES ('2', '1', '2024-07-10 11:30:00', NULL);
+VALUES ('2', '2', '2024-07-10 11:30:00', NULL);
 INSERT INTO railway.consultas (medico_id, paciente_id, fecha, motivo_cancelamiento)
-VALUES ('3', '1', '2024-07-15 14:00:00', NULL);
+VALUES ('3', '3', '2024-07-15 14:00:00', NULL);
 INSERT INTO railway.consultas (medico_id, paciente_id, fecha, motivo_cancelamiento)
-VALUES ('1', '1', '2024-08-01 10:00:00', NULL);
+VALUES ('4', '4', '2024-08-01 10:00:00', NULL);
 INSERT INTO railway.consultas (medico_id, paciente_id, fecha, motivo_cancelamiento)
-VALUES ('2', '1', '2024-08-12 15:30:00', NULL);
+VALUES ('5', '5', '2024-08-12 15:30:00', NULL);
 INSERT INTO railway.consultas (medico_id, paciente_id, fecha, motivo_cancelamiento)
-VALUES ('3', '1', '2024-08-20 13:45:00', NULL);
+VALUES ('6', '6', '2024-08-20 13:45:00', NULL);
 INSERT INTO railway.consultas (medico_id, paciente_id, fecha, motivo_cancelamiento)
-VALUES ('1', '2', '2024-07-08 12:00:00', NULL);
+VALUES ('7', '7', '2024-07-08 12:00:00', NULL);
 INSERT INTO railway.consultas (medico_id, paciente_id, fecha, motivo_cancelamiento)
-VALUES ('2', '3', '2024-07-22 16:00:00', NULL);
+VALUES ('8', '8', '2024-07-22 16:00:00', NULL);
 INSERT INTO railway.consultas (medico_id, paciente_id, fecha, motivo_cancelamiento)
-VALUES ('3', '4', '2024-08-05 09:15:00', NULL);
+VALUES ('9', '9', '2024-08-05 09:15:00', NULL);
 INSERT INTO railway.consultas (medico_id, paciente_id, fecha, motivo_cancelamiento)
-VALUES ('1', '5', '2024-08-25 11:30:00', NULL);
+VALUES ('10', '10', '2024-08-25 11:30:00', NULL);
